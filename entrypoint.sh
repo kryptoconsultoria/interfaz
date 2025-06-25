670d9faa98e7dc8fda68ab584c93e65297696822
Krypto <innovacion@krypto.com.co>
2025-06-25 14:49:53 -0500

    correccion archivo entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 echo "🔧 Cargando variables de entorno"
 [ -f .env ] && export $(cat .env | xargs)
 
-echo "Ejecutando migraciones..."
+echo "🛠 Aplicando migraciones..."
 python manage.py migrate --database=admin_db panel_principal
