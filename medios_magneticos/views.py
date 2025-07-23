c1e27feea5500413889bc4d62678697e82a00bce
oscarfelipe <felipe.castano@krypto.com.co>
2025-07-23 08:52:26 -0500

    arreglo vistas medios magneticos

--- medios_magneticos/views.py
+++ medios_magneticos/views.py
@@ -24,7 +24,7 @@ def json_response(success: bool, message: str, data=None, status=200):
 # Espera que SHAREPOINT_BASE_URL_MEDIOS esté definido en settings.py, por ejemplo:
 # SHAREPOINT_BASE_URL_MEDIOS = "https://empresa.sharepoint.com/sites/medios_magneticos/Shared Documents"
 
-def manejo_archivo_subida(request, field_name, subpath, allowed_ext=None):
+def manejo_archivo_subida(request, field_name, subpath,allowed_ext=None,aditional_path=None):
     file = request.FILES.get(field_name)
     if not file:
         return json_response(False, "No se recibió ningún archivo", status=400)
@@ -35,7 +35,7 @@ def manejo_archivo_subida(request, field_name, subpath, allowed_ext=None):
 
     # Construcción de la ruta completa en SharePoint
     base_url = settings.SHAREPOINT_BASE_URL_MEDIOS.rstrip('/')
-    sharepoint_path = f"{base_url}/{subpath}/{cliente}"
+    sharepoint_path = f"{base_url}/{subpath}/{cliente}{aditional_path}"
 
     # Validación de extensión
     ext = os.path.splitext(file.name)[1].lower()
@@ -51,14 +51,14 @@ def manejo_archivo_subida(request, field_name, subpath, allowed_ext=None):
         return json_response(False, f"Error interno: {str(e)}", status=500)
 
 # Borrar archivos del sharepoint
-def manejo_archivos_borrado(request, subpath, allowed_ext=None):
+def manejo_archivos_borrado(request, subpath,aditional_path=None):
     cliente = request.session.get('cliente_nombre')
     if not cliente:
         return json_response(False, "Cliente no definido en sesión", status=400)
 
     # Construcción de la ruta completa en SharePoint
     base_url = settings.SHAREPOINT_BASE_URL_MEDIOS.rstrip('/')
-    sharepoint_path = f"{base_url}/{subpath}/{cliente}"
+    sharepoint_path = f"{base_url}/{subpath}/{cliente}{aditional_path}"
 
     try:
         admin = AdministradorArchivos()
@@ -175,7 +175,7 @@ def anexos(request):
 def balances(request):
     # Ruta de SharePoint: https://empresa.sharepoint.com/sites/medios_magneticos/Shared Documents/insumos/insumos_{sistema}/balance_terceros/{cliente}
     sistema = request.session.get('sistema_nombre')
-    return manejo_archivo_subida(request, 'archivo', f'insumos/insumos_{sistema}/balance_terceros')
+    return manejo_archivo_subida(request, 'archivo', f'insumos/insumos_{sistema}',aditional_path=f'/balance_terceros')
 
 @csrf_exempt
 @login_required
@@ -183,7 +183,7 @@ def balances(request):
 def terceros(request):
     # Ruta de SharePoint: https://empresa.sharepoint.com/sites/medios_magneticos/Shared Documents/insumos/insumos_{sistema}/modelo_terceros/{cliente}
     sistema = request.session.get('sistema_nombre')
-    return manejo_archivo_subida(request, 'archivo', f'insumos/insumos_{sistema}/modelo_terceros')
+    return manejo_archivo_subida(request, 'archivo', f'insumos/insumos_{sistema}',aditional_path=f'/listado_terceros')
 
 @csrf_exempt
 @login_required
@@ -234,7 +234,7 @@ def anexos_borrado(request):
 def balances_borrado(request):
     # Ruta de SharePoint: https://empresa.sharepoint.com/sites/medios_magneticos/Shared Documents/insumos/insumos_{sistema}/balance_terceros/{cliente}
     sistema = request.session.get('sistema_nombre')
-    return manejo_archivos_borrado(request, f'insumos/insumos_{sistema}/balance_terceros')
+    return manejo_archivos_borrado(request, f'insumos/insumos_{sistema}',aditional_path=f'/balance_terceros')
 
 @csrf_exempt
 @login_required
@@ -242,7 +242,7 @@ def balances_borrado(request):
 def terceros_borrado(request):
     # Ruta de SharePoint: https://empresa.sharepoint.com/sites/medios_magneticos/Shared Documents/insumos/insumos_{sistema}/modelo_terceros/{cliente}
     sistema = request.session.get('sistema_nombre')
-    return manejo_archivos_borrado(request, f'insumos/insumos_{sistema}/modelo_terceros')
+    return manejo_archivos_borrado(request, f'insumos/insumos_{sistema}',aditional_path=f'/listado_terceros')
 
 @csrf_exempt
 @login_required
